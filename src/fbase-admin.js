8990586e8b8e1767e7b64cc06acfa6ca0851788e
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2021-10-16 19:49:35 +0900

    nookies 설치/ fbase-admin sdk 수정

--- src/fbase-admin.js
+++ src/fbase-admin.js
@@ -6,11 +6,7 @@ import serviceAccount from './fbase-admin.json';
 
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert({
-      privateKey: serviceAccount.private_key,
-      clientEmail: serviceAccount.client_email,
-      projectId: serviceAccount.project_id,
-    }),
+    credential: firebaseAdmin.credential.cert(serviceAccount),
     databaseURL: "https://play-and-6db77.firebaseio.com",
   });
 }
