c8a7e7333bca707b3efcb7b89970fdc5efec7441
Hyeon-Gwang <wkdgusrhkd@gmail.com>
2021-11-12 18:10:06 +0900

    src 폴더 정리

--- src/GroupAdminComponents/Config.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Config = () => {
-  return (
-    <h1>Admin Config</h1>
-  )
-}
-
-export default Config
--- src/GroupComponents/GameRecordForm.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const GameRecordForm = () => {
-  return (
-    <h1>폼</h1>
-  )
-}
-
-export default GameRecordForm
